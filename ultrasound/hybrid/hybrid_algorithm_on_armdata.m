bc115a534e037e43f353ceeeaaf45c7a7b9fcb28
Tim <timvdzee@hotmail.com>
2023-10-27 10:47:43 +0200

    some clean up and arm data option

--- ultrasound/hybrid/hybrid_algorithm_on_armdata.m
+++ ultrasound/hybrid/hybrid_algorithm_on_armdata.m
@@ -1,38 +1,36 @@
 clear all; close all; clc
-addpath(genpath('C:\Users\timvd\Documents\ultrasound-automated-algorithm'))
+addpath(genpath('C:\Users\timvd\Documents\ultrasound data\arm'))
+v = VideoReader('Arm_Image06.wmv');
 
-dates = {'3011', '1612','1601'};
-i = 1;
-date = dates{i};
-
-% cd(['C:\Users\timvd\OneDrive - University of Calgary\8. Ultrasound comparison - TBD\data\Test',date,'\videos']);
-% files = dir('*.mp4');
-% 
-% filename = files(1).name;
-% v = VideoReader(filename);
-
-cd('C:\Users\timvd\OneDrive - University of Calgary\8. Ultrasound comparison - TBD');
-v = VideoReader('short_clip.mp4');
-Is = read(v);
-% v = read('short_clip.mp4');
+i = 0;
+while hasFrame(v)
+    i = i+1;
+    disp(i)
+    Is(:,:,i) = rgb2gray(readFrame(v));
+end
 
 %% do TimTrack
 load('parms.mat')
-% parms.cut_image = 0;
-% 
-% parms.ROI = [239   936; 50   500]; % [0812]
+parms.cut_image = 0;
 % 
 parms.apo.deep.cut(1) = .35;
 % parms.apo.super.cut(1) = .03;
 
+parms.apo.deep.maxangle = 20;
+parms.apo.super.maxangle = 20;
+parms.extrapolation = 1;
+
 figure(1)
-parms.show = 0;
-j = 0;
+parms.show = 1;
 
-% [geofeatures, parms] = do_TimTrack('short_clip.mp4',parms);
+[geofeatures, parms] = do_TimTrack(Is,parms);
 
-load('short_clip_geofeatures.mat')
-% load(['asym_high_geofeatures_Test',date,'.mat'])
+%% save
+cd('C:\Users\timvd\Documents\ultrasound data\arm')
+save('Arm_Image06_geofeatures.mat');
+
+%% cut
+Is = Is(parms.ROI(2,1):parms.ROI(2,2), parms.ROI(1,1):parms.ROI(1,2),:);
 apox = [1 size(Is,2)];
 
 %% convert TimTrack to x,y points
@@ -45,7 +43,8 @@ for j = 1:length(geofeatures)
 end
 
 %% TimTrack video
-show_video(Is, xpos_tt, ypos_tt, dapo_tt, sapo_tt,'timtrack_video.gif')
+show_video(Is, xpos_tt, ypos_tt, dapo_tt, sapo_tt)
+% show_video(Is, xpos_tt, ypos_tt, dapo_tt, sapo_tt,'timtrack_armvideo.gif')
 
 %% optic flow
 if ishandle(2), close(2); end; figure(2);
@@ -59,8 +58,8 @@ sapo = polyval(geofeatures(1).super_coef, apox)';
 dapo = polyval(geofeatures(1).deep_coef, apox)';
 
 % next images
-j = 0;
-for j = 1:size(Is,4)
+% j = 0;
+for j = 1:size(Is,3)
 %      j = j+1;
      disp(j)
      
@@ -68,7 +67,8 @@ for j = 1:size(Is,4)
 %     I = rgb2gray(f);
 %     Ic = I(parms.ROI(2,1):parms.ROI(2,2), parms.ROI(1,1):parms.ROI(1,2));
 
-    Ic = rgb2gray(Is(:,:,:,j));
+%     Ic = rgb2gray(Is(:,:,:,j));
+    Ic = Is(:,:,j);
     x = 1:size(Ic,2);
     
     % optional: fix to edges
@@ -109,39 +109,37 @@ for j = 1:size(Is,4)
 end
 
 %% optic flow video
-show_video(Is, xpos, ypos, dapo, sapo,'opticflow_video.gif')
+show_video(Is, xpos, ypos, dapo, sapo,'opticflow_armvideo.gif')
 
 %% optic flow vs. timtrack
 close all; 
 
 figure(1)
-set(gcf,'name','Fascicle-aponeurosis intersection locations')
 subplot(221);
 plot(xpos(1,:)); hold on
-title('Horizontal deep intersection')
+title('Horizontal deep intersect')
 plot(xpos_tt(1,:)); hold on
 
 subplot(222);
 plot(xpos(2,:)); hold on
-title('Horizontal superficial intersection')
+title('Horizontal superficial intersect')
 plot(xpos_tt(2,:)); hold on
 
 subplot(223);
 plot(ypos(1,:)); hold on
-title('Vertical deep intersection')
+title('Vertical deep intersect')
 plot(ypos_tt(1,:)); hold on
 
 subplot(224);
 plot(ypos(2,:)); hold on
-title('Vertical superficial intersection')
+title('Vertical superficial intersect')
 plot(ypos_tt(2,:)); hold on
 
 figure(2);
-set(gcf,'name','Aponeurosis locations (at image borders)')
 subplot(221); 
 plot(dapo(1,:)); hold on
 plot(dapo_tt(1,:));
-title('Vertical deep left intersection')
+title('Vertical deep left intersect')
 
 subplot(222); 
 plot(sapo(1,:)); hold on
@@ -158,20 +156,7 @@ plot(sapo(2,:)); hold on
 plot(sapo_tt(2,:));
 title('Vertical superficial right')
 
-for j = 1:2
-    figure(j)
-for i = 1:4
-    subplot(2,2,i)
-    box off
-    xlabel('Sample #')
-    ylabel('Position (pixels)')
-end
-
-legend('TimTrack','Optic flow','location','best')
-legend box off
-end
-
-%% calculate difference vectors
+%% difference vectors
 clear dxpos dypos dsapo ddapo
 for i = 1:2
     dxpos(i,:) = diff(xpos(i,:));
@@ -182,8 +167,6 @@ for i = 1:2
 end
 
 %% state estimation vertical positions aponeuroses
-% do state estimation on vertical position
-
 alpha = .1;
 sapo_est = sapo_tt;
 dapo_est = dapo_tt;
@@ -208,9 +191,7 @@ subplot(222); plot(sapo_est(1,:));
 subplot(223); plot(dapo_est(2,:));
 subplot(224); plot(sapo_est(2,:));
 
-
 %% re-fit the first-orders
-% use estimates to recompute aponeurosis fits
 for j = 1:length(xpos)
     deep_coef(j,:) = polyfit(apox(:), dapo_est(:,j),1);
     super_coef(j,:) = polyfit(apox(:), sapo_est(:,j),1); 
@@ -219,35 +200,20 @@ for j = 1:length(xpos)
     super_coef_tt(j,:) = geofeatures(j).super_coef;
 end
 
-if ishandle(3), close(3); end; figure(3)
+figure(3)
 subplot(221); plot(deep_coef_tt(:,1)); hold on
-plot(deep_coef(:,1)); title('Deep aponeurosis slope')
+plot(deep_coef(:,1)); title('Deep coef slope')
 
 subplot(222); plot(super_coef_tt(:,1)); hold on
-plot(super_coef(:,1)); title('Superficial aponeurosis slope')
+plot(super_coef(:,1)); title('Super coef slope')
 
 subplot(223); plot(deep_coef_tt(:,2)); hold on
-plot(deep_coef(:,2)); title('Deep aponeurosis offset')
+plot(deep_coef(:,2)); title('Deep coef offset')
 
 subplot(224); plot(super_coef_tt(:,2)); hold on
-plot(super_coef(:,2)); title('Superficial aponeurosis offset')
+plot(super_coef(:,2)); title('Super coef offset')
 
-
-for i = 1:4
-    subplot(2,2,i)
-    box off
-    xlabel('Sample #')
-    ylabel('Position (pixels)')
-end
-
-legend('TimTrack','State estimation','location','best')
-legend box off
-
-
-%% state estimation superficial aponeurosis intersection point
-% state estimation for horizontal point
-% get vertical point from aponeurosis
-
+%% state estimation superficial intersection point
 alpha = .05;
 ypos_est = ypos;
 xpos_est = xpos;
@@ -273,10 +239,7 @@ figure(1)
 subplot(222); plot(xpos_est(2,:)); hold on
 subplot(224); plot(ypos_est(2,:)); hold on
 
-%% state estimation deep aponeurosis intersection point
-% state estimation for horizontal point
-% get vertical point from aponeurosis
-
+%% state estimation deep
 alpha = .1;
 
 for j = 1:length(dxpos)
@@ -307,26 +270,30 @@ subplot(221); plot(xpos_est(1,:)); hold on
 subplot(223); plot(ypos_est(1,:)); hold on
 
 %% evaluate results
-show_video(Is, xpos_est, ypos_est, dapo_est, sapo_est,'estimator_video.gif')
+show_video(Is, xpos_est, ypos_est, dapo_est, sapo_est)
 
-%%
-
-
-
-
-
-%% functions
+%% show video function 
 function[] = show_video(Is, xpos_est, ypos_est, dapo_est, sapo_est, GIF_filename)
     [n,m,~,~] = size(Is);
     apox = [1 m];
     p = 1;
     
     close all; figure(1)
-
-
     for i = 1:length(xpos_est)-1
+        faslen(i) = sqrt(diff(xpos_est(:,i)).^2 + diff(ypos_est(:,i)).^2);
+    end
 
-        data_padded = [ones(n,round(m/2),p) rgb2gray(Is(:,:,:,i)) ones(n,round(m/2),p)];     % zero padding
+    subplot(5,1,5);
+    plot(1:length(xpos_est)-1, faslen); 
+    h = line('xdata',[],'ydata',[],'marker','.','markersize',10);
+    xlabel('Sample number'); ylabel('Fascicle length (pixels)'); box off
+    
+    for i = 1:length(xpos_est)-1
+       
+
+        data_padded = [ones(n,round(m/2),p) Is(:,:,i) ones(n,round(m/2),p)];     % zero padding
+
+        subplot(5,1,1:4)
         imshow(data_padded,'xdata',[-round(m/2) round(1.5*m)], 'ydata',[1 n]); hold on
 
         plot(xpos_est(:,i),ypos_est(:,i),'ro-','linewidth',2)
@@ -334,6 +301,9 @@ function[] = show_video(Is, xpos_est, ypos_est, dapo_est, sapo_est, GIF_filename
         plot(apox, sapo_est(:,i), 'b', 'linewidth',2);
 
         hold off; drawnow
+        
+        subplot(5,1,5); set(h, 'xdata', i, 'ydata', faslen(i)); hold off
+        
         
         if nargin > 5
             if i == 1
